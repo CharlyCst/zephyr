CharlyCst/zephyr

commit a9929d5f1ccb080e3bca359062b9726e1129959b

Add frontend suport for f32 and f64

src/ast/parse.rs
@@ -349,6 +349,13 @@ impl<'a> Parser<'a> {
             return Err(());
         }
         let result = self.result();
+        if !self.next_match_report(
+            TokenType::LeftBrace,
+            "A left brace '{' is expected at the beginning of the function body.",
+        ) {
+            self.synchronize_fun();
+            return Err(());
+        }
         let block = self.block()?;
         self.consume_semi_colon();
         Ok(Function {
@@ -829,9 +836,15 @@ impl<'a> Parser<'a> {
         let token = self.advance();
 
         match &token.t {
-            TokenType::NumberLit(n) => Ok(Expression::Literal {
+            TokenType::IntegerLit(n) => Ok(Expression::Literal {
                 value: Value::Integer {
                     val: *n,
+                    loc: token.loc,
+                },
+            }),
+            TokenType::FloatLit(x) => Ok(Expression::Literal {
+                value: Value::Float {
+                    val: *x,
                     loc: token.loc,
                 },
             }),
@@ -855,7 +868,12 @@ impl<'a> Parser<'a> {
                 }
                 Ok(expr)
             }
-            _ => Err(()),
+            _ => {
+                let loc = token.loc;
+                self.err
+                    .report(loc, String::from("Expected a value or identifier."));
+                Err(())
+            }
         }
     }
 

src/ast/ast.rs
@@ -3,6 +3,7 @@ use std::fmt;
 
 pub enum Value {
     Integer { val: u64, loc: Location },
+    Float { val: f64, loc: Location },
     Boolean { val: bool, loc: Location },
 }
 
@@ -230,6 +231,7 @@ impl fmt::Display for Expression {
                 Value::Boolean { val: true, .. } => write!(f, "true"),
                 Value::Boolean { val: false, .. } => write!(f, "false"),
                 Value::Integer { val: n, .. } => write!(f, "{}", n),
+                Value::Float { val: x, .. } => write!(f, "{}", x),
             },
             Expression::Call { fun, args } => write!(
                 f,

src/ast/scan.rs
@@ -39,7 +39,7 @@ impl<'a> Scanner<'a> {
         .iter()
         .cloned()
         .collect();
-        
+
         // f_id MUST exist
         let code = error_handler.get_file(f_id).unwrap();
 
@@ -162,7 +162,8 @@ impl<'a> Scanner<'a> {
     /// return
     fn check_stmt_ender(&mut self, token: &Token) {
         let candidate = match token.t {
-            TokenType::NumberLit(_) => true,
+            TokenType::IntegerLit(_) => true,
+            TokenType::FloatLit(_) => true,
             TokenType::BooleanLit(_) => true,
             TokenType::StringLit(_) => true,
             TokenType::Identifier(_) => true,
@@ -235,19 +236,37 @@ impl<'a> Scanner<'a> {
 
     /// Consumes consecutive digit characters and push a number token
     fn number(&mut self, tokens: &mut Vec<Token>) {
+        let mut is_integer = true;
         while !self.is_at_end() && self.peek().is_digit(RADIX) {
             self.advance();
+        }
+        if self.peek() == '.' {
+            self.advance();
+            is_integer = false;
+            while !self.is_at_end() && self.peek().is_digit(RADIX) {
+                self.advance();
+            }
         }
         let str_val = self.code[self.start..self.current]
             .iter()
             .cloned()
             .collect::<String>();
-        match str_val.parse::<u64>() {
-            Ok(n) => self.add_token(tokens, TokenType::NumberLit(n)),
-            Err(_) => self.err.report(
-                self.get_loc(),
-                format!("Could not parse {} as a number", str_val),
-            ),
+        if is_integer {
+            match str_val.parse::<u64>() {
+                Ok(n) => self.add_token(tokens, TokenType::IntegerLit(n)),
+                Err(_) => self.err.report(
+                    self.get_loc(),
+                    format!("Could not parse {} as an integer.", str_val),
+                ),
+            }
+        } else {
+            match str_val.parse::<f64>() {
+                Ok(x) => self.add_token(tokens, TokenType::FloatLit(x)),
+                Err(_) => self.err.report(
+                    self.get_loc(),
+                    format!("Could not parse {} as a float.", str_val),
+                ),
+            }
         }
     }
 

src/ast/tokens.rs
@@ -32,7 +32,8 @@ pub enum TokenType {
 
     // Literals
     Identifier(String),
-    NumberLit(u64),
+    IntegerLit(u64),
+    FloatLit(f64),
     BooleanLit(bool),
     StringLit(String),
 

src/mir/ast_to_mir.rs
@@ -245,6 +245,17 @@ impl<'a> MIRProducer<'a> {
                     };
                     stmts.push(Statement::Const { val: val })
                 }
+                V::Float { val, t_id, .. } => {
+                   let t = get_type(*t_id, s)?;
+                   let val = match t {
+                        Type::F32 => Value::F32(*val as f32),
+                        Type::F64 => Value::F64(*val),
+                        _ => {
+                            return Err(String::from("Float constant of non float type."));
+                        }
+                   };
+                   stmts.push(Statement::Const { val: val })
+                },
                 V::Boolean { val, .. } => stmts.push(Statement::Const {
                     val: Value::I32(if *val { 1 } else { 0 }),
                 }),

src/mir/names.rs
@@ -74,6 +74,11 @@ pub enum Value {
         loc: Location,
         t_id: TypeId,
     },
+    Float {
+        val: f64,
+        loc: Location,
+        t_id: TypeId,
+    },
     Boolean {
         val: bool,
         loc: Location,
@@ -127,6 +132,7 @@ impl Expression {
             Expression::Literal { value } => match value {
                 Value::Boolean { loc, .. } => *loc,
                 Value::Integer { loc, .. } => *loc,
+                Value::Float { loc, .. } => *loc,
             },
             Expression::Function { loc, .. } => *loc,
             Expression::Unary { loc, .. } => *loc,

src/mir/resolver.rs
@@ -537,6 +537,17 @@ impl<'a> NameResolver<'a> {
                     };
                     Ok((expr, fresh_t_id))
                 }
+                ast::Value::Float { val, loc } => {
+                   let fresh_t_id = state.types.fresh(*loc, vec![Type::F32, Type::F64]);
+                   let expr = Expression::Literal {
+                        value: Value::Float {
+                            val: *val,
+                            loc: *loc,
+                            t_id: fresh_t_id,
+                        },
+                   };
+                   Ok((expr, fresh_t_id))
+                },
                 ast::Value::Boolean { val, loc } => {
                     let expr = Expression::Literal {
                         value: Value::Boolean {

src/mir/type_check.rs
@@ -1,5 +1,5 @@
 use super::names::{Declaration, Function, NameStore};
-use super::types::id::T_ID_INTEGER;
+use super::types::id::{T_ID_FLOAT, T_ID_INTEGER};
 use super::types::{ConstraintStore, Type, TypeConstraint, TypeStore, TypeVarStore};
 use super::{ResolvedProgram, TypedProgram};
 use crate::error::{ErrorHandler, Location};
@@ -56,6 +56,7 @@ impl<'a> TypeChecker<'a> {
     /// satisfyied.
     fn build_store(&mut self, var_store: &TypeVarStore) -> TypeStore {
         let integers = var_store.get(T_ID_INTEGER);
+        let floats = var_store.get(T_ID_FLOAT);
         let mut store = TypeStore::new();
         for var in var_store {
             if var.types.len() == 1 {
@@ -70,9 +71,13 @@ impl<'a> TypeChecker<'a> {
                 // Choose arbitrary type if applicable
                 if var.types == integers.types {
                     store.put(Type::I64);
+                } else if var.types == floats.types {
+                    store.put(Type::F64);
                 } else {
                     // TODO: improve error handling...
-                    self.err.report_no_loc(String::from("Could not infer type"))
+                    println!("{:?}", floats.types);
+                    println!("{:?}", var.types);
+                    self.err.report(var.loc, String::from("Could not infer type"))
                 }
             }
         }

commit c62d7e5ce8733c59c47b82e05d2358a2e4b6d4ae

Fix a regression introduced in commit a9929d5f1ccb080e3bca359062b9726e1129959b

src/ast/parse.rs
@@ -868,12 +868,7 @@ impl<'a> Parser<'a> {
                 }
                 Ok(expr)
             }
-            _ => {
-                let loc = token.loc;
-                self.err
-                    .report(loc, String::from("Expected a value or identifier."));
-                Err(())
-            }
+            _ => Err(()),
         }
     }
 